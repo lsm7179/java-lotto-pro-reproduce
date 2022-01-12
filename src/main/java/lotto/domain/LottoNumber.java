30d299dbf551d6e5dc19e2d606b535e21c2d1d43
이승민 <sml7179@naver.com>
2022-01-12 13:37:19 +0900

    feat: 로또 숫자 1 ~ 45 검증

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,7 +3,18 @@ package lotto.domain;
 public class LottoNumber {
     private int number;
 
-    public LottoNumber(int number) {
+    private LottoNumber(int number) {
         this.number = number;
+    }
+
+    public static LottoNumber from(int number) {
+        validateSize(number);
+        return new LottoNumber(number);
+    }
+
+    private static void validateSize(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -4,14 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
 
     @DisplayName("로또 번호를 생성")
     @Test
     void lottoNumberCreate() {
-        LottoNumber lottoNumber = new LottoNumber(3);
+        LottoNumber lottoNumber = LottoNumber.from(3);
 
         assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("1 ~ 45 사이의 숫자가 아니면 예외처리")
+    @Test
+    void sizeError() {
+        assertThatThrownBy(
+                () -> LottoNumber.from(46)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(
+                () -> LottoNumber.from(0)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
